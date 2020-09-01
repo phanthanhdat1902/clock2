99d02751b9675f5b3f877e729eb61f18c4e6b53e
phanthanhdat1902 <38830161+phanthanhdat1902@users.noreply.github.com>
2020-09-01 10:38:09 +0700

    edit udp client

--- UDP/UDPClient/udpclient.go
+++ UDP/UDPClient/udpclient.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
 func main() {
+	var saddr net.UDPAddr
+	saddr.Port = 8888
+	saddr.IP = net.ParseIP("127.0.0.1")
+	server, _ := net.DialUDP("udp", nil, &saddr)
+	fmt.Println("running")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		msg, _ := reader.ReadString('\n')
+		server.Write([]byte(msg))
+	}
 }
--- UDP/UDPServer/serverclient.go
+++ UDP/UDPServer/serverclient.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"runtime"
+	"strings"
 )
 
 func main() {
@@ -12,16 +15,28 @@ func main() {
 	saddr.Port = 8888
 	connection, _ := net.ListenUDP("udp", &saddr)
 	for i := 0; i < runtime.NumCPU(); i++ {
+		fmt.Println(runtime.NumCPU())
 		go handleConnect(connection)
 	}
 	for {
-
+		reader := bufio.NewReader(os.Stdin)
+		msg, _ := reader.ReadString('\n')
+		if strings.Compare(msg, "close server\n") == 0 {
+			break
+		}
 	}
 }
 func handleConnect(connection *net.UDPConn) {
 	buffer := make([]byte, 1024)
 	for {
+		memset(buffer)
 		_, client, _ := connection.ReadFromUDP(buffer)
+		connection.WriteToUDP(buffer, client)
 		fmt.Println(client.Port, client.IP, string(buffer))
+	}
+}
+func memset(des []byte) {
+	for i := range des {
+		des[i] = byte(0)
 	}
 }
